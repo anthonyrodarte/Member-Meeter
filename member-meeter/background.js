e7570c8448e1244ed8fb9fcd54e3904f13c5ebfb
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-03-28 15:21:59 -0700

    Added simple layout for landing. closes #1

---
+++ member-meeter/background.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Heading() {
+  return <h1>Member Meeter</h1>
+}
--- member-meeter/index.js
+++ member-meeter/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Users from './featured-users.js'
+import Users from './users.js'
+import Heading from './background.js'
 
+ReactDOM.render(<Heading />, document.querySelector('#header'))
 ReactDOM.render(<Users />, document.querySelector('#app'))
