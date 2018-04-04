869b882125b9a2f1a1319bc9226440aec6be20b8
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-04-04 13:39:19 -0700

    Organized modal layout.

--- musician-details.js
+++ musician-details.js
@@ -14,21 +14,44 @@ export default class MusicianDetails extends React.Component {
   }
   render() {
     return (
-      <Modal visible={this.state.open} onClickBackdrop={this.closeDetails}>
+      <Modal
+        visible={this.state.open}
+        dialogClassName="modal-dialog-centered modal-lg"
+        onClickBackdrop={this.closeDetails}
+      >
         <div className="modal-header">
-          <h5 className="modal-title">Test Modal</h5>
-        </div>
-        <div className="modal-body">
-          <p>Content</p>
-        </div>
-        <div className="modal-footer">
           <button
             type="button"
-            className="btn btn-secondary"
+            className="close"
+            aria-label="Close"
             onClick={this.closeDetails}
           >
-            Close
+            <span aria-hidden="true">&times;</span>
           </button>
+        </div>
+        <div className="modal-body container text-center">
+          <div className="row">
+            <div className="col-5">
+              <img
+                src="testimg.jpg"
+                className="rounded img-thumbnail"
+                alt="Responsive image"
+              />
+              <h5 className="my-3">George Bluth</h5>
+              <h6 className="my-3">Anaheim, CA</h6>
+              <h6 className="my-3">Guitar</h6>
+            </div>
+            <div className="col-7">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                placerat mattis maximus. Suspendisse et metus mollis, tristique
+                felis at, vehicula risus. Nunc vel nibh ultricies, ornare urna
+                ac, molestie ipsum. Maecenas rutrum vestibulum elit sit amet
+                volutpat. Integer vel augue placerat, gravida mi ac, elementum
+                purus.
+              </p>
+            </div>
+          </div>
         </div>
       </Modal>
     )
