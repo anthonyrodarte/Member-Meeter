92a5d0f980c4e8e16254e7744697b945b7fc7702
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-04-05 13:07:50 -0700

    Added selected musician to Display component state. #5

--- users.js
+++ users.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import MusicianDetails from './musician-details.js'
 
 export default class Users extends React.Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       <div className="row d-flex align-content-center flex-wrap">
@@ -10,7 +14,13 @@ export default class Users extends React.Component {
             className="card text-center bg-light text-dark"
             style={{ width: this.props.width, margin: this.props.margin }}
           >
-            <img className="card-img-top" src={$user.photo} alt="User Photo" />
+            <img
+              className="card-img-top"
+              src={$user.photo}
+              alt="User Photo"
+              id={$user.id}
+              onClick={this.props.selector}
+            />
             <div className="card-body">
               <h5 className="card-title">
                 {$user.firstName} {$user.lastName}
